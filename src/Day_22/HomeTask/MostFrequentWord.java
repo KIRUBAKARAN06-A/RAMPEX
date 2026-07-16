e17cc4adbe9901488b808013e9f8d8f27175511a
Kirubakaran.A <a.karankiruba6607@gmail.com>
2026-07-16 09:25:36 +0530

    program created this java program using loops,arrays,class,oops,function,String,oops,data structure and algorithm

---
+++ src/Day_22/HomeTask/MostFrequentWord.java
@@ -0,0 +1,20 @@
+package Day_22.HomeTask;
+import java.util.*;
+import java.util.stream.*;
+public class MostFrequentWord {
+          public static void main(String[] args) {
+
+            String text = "kiruba karan kiruba hari kiruba";
+
+            Map<String, Long> map = Arrays.stream(text.toLowerCase().split(" "))
+                    .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
+
+            Map.Entry<String, Long> max = map.entrySet()
+                    .stream()
+                    .max(Map.Entry.comparingByValue())
+                    .get();
+
+            System.out.println(max.getKey() + " -> " + max.getValue());
+        }
+    }
+
---
+++ src/Day_22/HomeTask/SortedArray.java
@@ -0,0 +1,17 @@
+package Day_22.HomeTask;
+import java.util.Arrays;
+public class SortedArray {
+    public static void main(String[] args) {
+
+            int[] nums = {-4,-1,0,3,10};
+
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = nums[i] * nums[i];
+            }
+
+            Arrays.sort(nums);
+
+            System.out.println(Arrays.toString(nums));
+        }
+    }
+
